65e206fd23e64e35cb1c020830f0b1a5a0818bbd
Thomas Margraf <thomas.margraf@areto.de>
2026-04-04 11:08:36 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {
+    self,
+    nix-darwin,
+    nixpkgs,
+  }: let
+    configuration = {pkgs, ...}: {
+      nix.enable = false;
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+        nixd
+        alejandra
+        ruff
+        uv
+        ty
+        bat
+        fzf
+        fzf-zsh-plugin
+        zsh-fzf-tab
+        neovim
+        zoxide
+        nmap
+        tree-sitter
+        carapace
+        carapace-bridge
+        gh
+        eza
+      ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      
+      # Enable Touch ID auth for sudo 
+      security.pam.services.sudo_local.touchIdAuth = true;
+      
+      # Set the NIX?PATH for nixd
+      nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+    };
+  in {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."TMA-M4" = nix-darwin.lib.darwinSystem {
+      modules = [configuration];
+      system.defaults = {
+        nix.linux-builder.enable = true;
+        dock.autohide = true;
+        dock.mru-spaces = false;
+        finder.AppleShowAllExtensions = true;
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.LoginwindowText = "Managed by areto and Flatballer";
+        screencapture.location = "~/Pictures/screenshots";
+        screensaver.askForPasswordDelay = 10;
+      };
+    };
+  };
+}
